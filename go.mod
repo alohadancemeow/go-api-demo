6eaf2d197a138b8d16077ed0da1bbea9475ebbc3
Alohadancemeow <nawongsak@gmail.com>
2025-04-17 16:18:02 +0700

    feat: add template rendering, file upload, and env support
    Introduce HTML template rendering using Fiber's template engine, add file upload functionality, and load environment variables from .env file. These changes enhance the application's capabilities by supporting dynamic content rendering, file handling, and secure configuration management.

--- go.mod
+++ go.mod
@@ -7,8 +7,12 @@ require (
 	github.com/fxamacker/cbor/v2 v2.8.0 // indirect
 	github.com/gofiber/fiber/v3 v3.0.0-beta.4 // indirect
 	github.com/gofiber/schema v1.3.0 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/template/html/v2 v2.1.3 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/template/html/v2"
+	"github.com/joho/godotenv"
 )
 
 // Book struct to hold book data
@@ -16,8 +19,20 @@ type Book struct {
 var books []Book // Slice to store books
 
 func main() {
+
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	engine := html.New("./views", ".html")
+
 	// Initialize a new Fiber app
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		// Pass in Views Template Engine
+		Views: engine,
+	})
 
 	// Initialize in-memory data
 	books = append(books, Book{ID: 1, Title: "1984", Author: "George Orwell"})
@@ -36,6 +51,43 @@ func main() {
 	app.Put("/book/:id", updateBook)
 	app.Delete("/book/:id", deleteBook)
 
+	app.Post("/upload", uploadFile)
+
+	// Render HTML template
+	app.Get("/html-test", func(c fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title":       "Go Fiber Template Example",
+			"Description": "An example template",
+			"Greeting":    "Hello, World!",
+		})
+	})
+
+	// Get environment variables
+	app.Get("/env", getEnv)
+
 	// Start the server on port 3000
 	log.Fatal(app.Listen(":3000"))
+}
+
+// Upload Image file
+func uploadFile(c fiber.Ctx) error {
+	file, err := c.FormFile("image")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	err = c.SaveFile(file, "./uploads/"+file.Filename)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.SendString("File uploaded successfully!")
+}
+
+// get environment variables
+func getEnv(c fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"SECRET_KEY": os.Getenv("SECRET_KEY"),
+	})
 }
