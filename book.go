e61402ca3e184c249df8a489c43d54d9eb5a4661
Alohadancemeow <nawongsak@gmail.com>
2025-04-17 15:36:58 +0700

    feat: add basic CRUD API for books using Fiber framework
    This commit introduces a new feature that implements a basic CRUD API for managing books. The API is built using the Fiber framework and includes handlers for creating, reading, updating, and deleting books. Additionally, the necessary dependencies have been added to the `go.mod` file to support the Fiber framework and related utilities.

---
+++ book.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"slices"
+	"strconv"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+// Handlers
+func getBooks(c fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+func getBook(c fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	for _, book := range books {
+		if book.ID == id {
+			return c.JSON(book)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func createBook(c fiber.Ctx) error {
+	book := new(Book)
+
+	if err := c.Bind().Body(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	// Find the maximum ID currently in use
+	maxID := 0
+	for _, b := range books {
+		if b.ID > maxID {
+			maxID = b.ID
+		}
+	}
+
+	// Set the new book's ID to maxID + 1
+	// Prevent duplicate IDs
+	book.ID = maxID + 1
+	books = append(books, *book)
+
+	return c.JSON(book)
+}
+
+func updateBook(c fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	bookUpdate := new(Book)
+	if err := c.Bind().Body(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			book.Title = bookUpdate.Title
+			book.Author = bookUpdate.Author
+			books[i] = book
+			return c.JSON(book)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func deleteBook(c fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			books = slices.Delete(books, i, i+1)
+			return c.SendStatus(fiber.StatusNoContent)
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
+}
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+// Book struct to hold book data
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book // Slice to store books
 
 func main() {
-	fmt.Println("Hello World!")
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	// Initialize in-memory data
+	books = append(books, Book{ID: 1, Title: "1984", Author: "George Orwell"})
+	books = append(books, Book{ID: 2, Title: "The Great Gatsby", Author: "F. Scott Fitzgerald"})
+
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
+
+	// CRUD routes
+	app.Get("/books", getBooks)
+	app.Get("/book/:id", getBook)
+	app.Post("/book", createBook)
+	app.Put("/book/:id", updateBook)
+	app.Delete("/book/:id", deleteBook)
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
